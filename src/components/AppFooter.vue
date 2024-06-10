8fa66da4aa78d9505e91075804ae0effe01b77ee
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-10 17:42:22 +0200

    created components and imported them in App.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data(){
+        return {
+            message: 'Footer',
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ message }} </h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data(){
+        return {
+            message: 'Main',
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ message }} </h1>
+</template>
+
+<style scoped>
+
+</style>
