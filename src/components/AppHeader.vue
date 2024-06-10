1b5bf5713e5248b05f4b894365160a0323611078
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-10 18:35:47 +0200

    added AppHeader general layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,9 +9,58 @@ data(){
 </script>
 
 <template>
-    <h1> {{ message }} </h1>
+    <header>
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="DC Comics Logo">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li class="active"><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
 
+    header {
+        background-color: white;
+    }
+
+    div.container {
+        @include my-container();
+        @include row-between-center();
+        }
+
+    img {
+        width: 85px
+    }
+
+    nav ul {
+        @include row-center();
+
+        li {
+            text-transform: uppercase;
+            margin: 0 1rem;
+            padding: 3rem 0;
+            border-bottom: 5px solid transparent;
+
+            &.active {
+                border-bottom-color: $primary-color;
+            }
+        }
+    }
 </style>
