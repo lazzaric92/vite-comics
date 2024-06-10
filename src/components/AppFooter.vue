7b9642779bc9a700871b330d7e26be4d11ca7212
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-10 19:47:36 +0200

    added .footer-nav general layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,7 +12,86 @@ export default {
     <footer>
         <section class="footer-nav">
             <div class="container">
-
+                <div class="nav-wrapper">
+                    <nav>
+                        <h2>DC Comics</h2>
+                        <ul>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <nav>
+                        <h2>DC Comics</h2>
+                        <ul>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <nav>
+                        <h2>DC Comics</h2>
+                        <ul>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <nav>
+                        <h2>DC Comics</h2>
+                        <ul>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                            <li>
+                                <a href="#">link</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <img src="../assets/img/dc-logo-bg.png" alt="DC Comics Logo">
             </div>
         </section>
         <section class="footer">
@@ -22,7 +101,9 @@ export default {
                     <h2>Follow us</h2>
                     <ul>
                         <li>
-                            <img src="../assets/img/footer-facebook.png" alt="Facebook Icon">
+                            <a href="#">
+                                <img src="../assets/img/footer-facebook.png" alt="Facebook Icon">
+                            </a>
                         </li>
                         <li>
                             <img src="../assets/img/footer-twitter.png" alt="Twitter Icon">
@@ -50,45 +131,74 @@ export default {
 
     div.container {
         @include my-container();
+        @include row-between-center();
+        height: 100%;
+
     }
 
-    footer {
-        background-image: url(../assets/img/footer-bg.jpg)
+    section.footer-nav{
+        background-image: url(/src/assets/img/footer-bg.jpg);
+        background-size: cover;
+        height: 375px;
+        overflow-y: clip;
+
+        .nav-wrapper {
+            display: flex;
+            flex-flow: column wrap;
+            height: 100%;
+            padding: 3rem 0;
+
+            nav {
+                margin-right: 3rem;
+                margin-bottom: .75rem;
+
+                h2 {
+                    margin-bottom: 1rem;
+                }
+
+                li {
+                    margin-bottom: .3rem;
+                }
+            }
+        }
+
+        a {
+            color: $font-grey;
+        }
     }
 
     section.footer {
         background-color: $bg-footer;
         padding: 2rem 0;
 
-        div.container {
-            @include row-between-center;
-
-            button {
-                text-transform: uppercase;
-                background-color: transparent;
-                color: white;
-                padding: 1rem;
-                border: 2px solid $primary-color;
-            }
-        }
-    }
-
-    .socials {
-        @include row-center;
-
-        h2 {
+        button {
             text-transform: uppercase;
-            color: $primary-color;
-            margin-right: 1.5rem
+            background-color: transparent;
+            color: white;
+            padding: 1rem;
+            border: 2px solid $primary-color;
+            cursor: pointer;
         }
 
-        ul {
-            @include row-center;
+        .socials {
+        @include row-center();
 
-            li {
-                margin: 0 .75rem;
+            h2 {
+                text-transform: uppercase;
+                color: $primary-color;
+                margin-right: 1.5rem
+            }
+
+            ul {
+                @include row-center();
+
+                li {
+                    margin: 0 .75rem;
+                }
             }
         }
     }
+
+    
 
 </style>
