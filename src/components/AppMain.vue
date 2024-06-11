7da7936780354ebf4c2ef83845b2220e1c36db04
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-11 12:52:55 +0200

    added ShopBanner component with dynamic images

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,40 +1,13 @@
 <script>
+import ShopBanner from './ShopBanner.vue'
 export default {
     data(){
         return {
-            shopLinks: [
-                {
-                    id: 1,
-                    title: 'Digital Comics',
-                    image: './src/assets/img/buy-comics-digital-comics.png',
-                    url: '#'
-                },
-                {
-                    id: 2,
-                    title: 'Merchandise',
-                    image: './src/assets/img/buy-comics-merchandise.png',
-                    url: '#'
-                },
-                {
-                    id: 3,
-                    title: 'Subscription',
-                    image: './src/assets/img/buy-comics-subscriptions.png',
-                    url: '#'
-                },
-                {
-                    id: 4,
-                    title: 'Comic Shop Locator',
-                    image: './src/assets/img/buy-comics-shop-locator.png',
-                    url: '#'
-                },
-                {
-                    id: 5,
-                    title: 'DC Power Visa',
-                    image: './src/assets/img/buy-dc-power-visa.svg',
-                    url: '#'
-                }
-            ]
+            
         }
+    },
+    components: {
+        ShopBanner,
     }
 }
 </script>
@@ -47,14 +20,7 @@ export default {
             </div>
         </section>
         <section class="shop-banner">
-            <div class="container">
-                <ul>
-                    <li v-for="link in shopLinks" :key="link.id">
-                        <img :src="link.image" :alt="link.title">
-                        <a :href="link.url"> {{ link.title }} </a>
-                    </li>
-                </ul>
-            </div>
+            <ShopBanner />
         </section>
     </main>
 </template>
@@ -74,32 +40,6 @@ export default {
 
     .shop-banner {
         background-color: $primary-color;
-
-        ul {
-            @include row-between-center();
-            padding: 3rem;
-
-            li {
-                @include row-center();
-                width: calc(100% / 6);
-            }
-
-            img {
-                margin-right: 1rem;
-                width: 50px;
-                height: 55px;
-            }
-
-            a {
-                text-transform: uppercase;
-                color: white;
-                font-size: .85rem;
-            }
-
-            a:hover{
-                text-decoration: underline;
-            }
-        }
     }
     
 </style>
---
+++ src/components/ShopBanner.vue
@@ -0,0 +1,92 @@
+<script>
+export default{
+    data(){
+        return {
+            shopLinks: [
+                {
+                    id: 1,
+                    title: 'Digital Comics',
+                    image: 'buy-comics-digital-comics.png',
+                    url: '#'
+                },
+                {
+                    id: 2,
+                    title: 'Merchandise',
+                    image: 'buy-comics-merchandise.png',
+                    url: '#'
+                },
+                {
+                    id: 3,
+                    title: 'Subscription',
+                    image: 'buy-comics-subscriptions.png',
+                    url: '#'
+                },
+                {
+                    id: 4,
+                    title: 'Comic Shop Locator',
+                    image: 'buy-comics-shop-locator.png',
+                    url: '#'
+                },
+                {
+                    id: 5,
+                    title: 'DC Power Visa',
+                    image: 'buy-dc-power-visa.svg',
+                    url: '#'
+                }
+            ]
+        }
+    },
+    methods: {
+        getImagePath: function(img){
+            return new URL(`../assets/img/${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <ul>
+            <li v-for="link in shopLinks" :key="link.id">
+                <img :src="getImagePath(link.image)" :alt="link.title">
+                <a :href="link.url"> {{ link.title }} </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+
+    div.container {
+        @include my-container();
+    }
+
+    ul {
+        @include row-between-center();
+        padding: 3rem;
+
+        li {
+            @include row-center();
+            width: calc(100% / 6);
+        }
+
+        img {
+            margin-right: 1rem;
+            width: 50px;
+            height: 55px;
+        }
+
+        a {
+            text-transform: uppercase;
+            color: white;
+            font-size: .85rem;
+        }
+
+        a:hover{
+            text-decoration: underline;
+        }
+    }
+</style>
